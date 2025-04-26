142e01ce529a851e50ee071e8bbb822d6d8a72d0
IanPedroBorges <ianhudback@gmail.com>
2025-04-26 02:44:49 -0300

    refactor: retirando a aunteticação

--- apiToDo/Controllers/TarefasController.cs
+++ apiToDo/Controllers/TarefasController.cs
@@ -67,7 +67,6 @@ namespace apiToDo.Controllers
             }
         }
 
-        [Authorize]
         [HttpPost]
         public ActionResult<TarefaDTO> CreateNewTarefa([FromBody] TarefaDTO Request)
         {
